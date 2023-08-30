dff29f7d46f954f92033e4e6c7d13ee375c799ba
DmitriyShelihov <142345381+DmitriyShelihov@users.noreply.github.com>
2023-08-30 12:58:02 +0300

    Update assertion.h

--- assertion.h
+++ assertion.h
@@ -15,7 +15,7 @@ const short unsigned int FOREGROUND_GREY = FOREGROUND_BLUE | FOREGROUND_RED | FO
 #define POSITION_IN_CODE __FILE__, __PRETTY_FUNCTION__, __LINE__  ///< a define that combines three position macros in the code
 
 /*!
-\brief a define that checks a variable for an error.
+\brief a define of variable assertion.
 */
 
 #define CONDITIONAL_COMPILATION
@@ -24,15 +24,15 @@ const short unsigned int FOREGROUND_GREY = FOREGROUND_BLUE | FOREGROUND_RED | FO
     #define cooler_assert(condition, error_code)                                                                              \
         if (condition)                                                                                                        \
             {                                                                                                                 \
-            set_colour(FOREGROUND_RED);                                         \
+            set_colour(FOREGROUND_RED);                                                                                       \
             fprintf (stderr, "Assert %s in file %s in %s:%d failed\n", #condition, POSITION_IN_CODE);                         \
-            set_colour(FOREGROUND_GREY); \
-            call_errors (error_code);                                                                                          \
+            set_colour(FOREGROUND_GREY);                                                                                      \
+            call_errors (error_code);                                                                                         \
             return error_code;                                                                                                \
             }
 #else
-    #define cooler_assert(condition, error_code) \
-        ;                                        \
+    #define cooler_assert(condition, error_code)                                               \
+        ;                                                                                      \
 #endif
 #endif
 
